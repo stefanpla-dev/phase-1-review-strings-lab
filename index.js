2ca201f732f1e54b4a2fd091908c4a18044c8efc
Stefan Pla <spla@icloud.com>
2024-01-29 15:44:32 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Stefan";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
